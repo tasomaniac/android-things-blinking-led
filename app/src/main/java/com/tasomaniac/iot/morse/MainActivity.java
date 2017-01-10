1d37514a63c57bfa983beff9d140d5e3bd856735
Said Tahsin Dane <tasomaniac@gmail.com>
2017-01-10 22:13:55 +0100

    Remove the javadoc explaining the sample

--- app/src/main/java/com/tasomaniac/iot/morse/MainActivity.java
+++ app/src/main/java/com/tasomaniac/iot/morse/MainActivity.java
@@ -28,24 +28,6 @@ import java.io.IOException;
 
 import timber.log.Timber;
 
-/**
- * Skeleton of the main Android Things activity. Implement your device's logic
- * in this class.
- * <p>
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- * <p>
- * <pre>{@code
- * PeripheralManagerService service = new PeripheralManagerService();
- * mLedGpio = service.openGpio("BCM6");
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
- * mLedGpio.setValue(true);
- * }</pre>
- * <p>
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- */
 public class MainActivity extends Activity {
 
     private Gpio gpio;
